56bfa27b2bb8b1c5c2f9d65270cb988a0d020939
Muhammad Irfan <mrfansi@outlook.com>
2025-01-30 15:20:34 +0700

    remove test

--- tests/Enums/QrisSourceTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Mrfansi\Xendit\Enums\QrisSource;
-
-test('qris source has correct values', function () {
-    expect(QrisSource::OVO->value)->toBe('OVO')
-        ->and(QrisSource::GOPAY->value)->toBe('GOPAY');
-});
